4a3ac66cfb6c9dc9fecd24f0c697615a75aafcde
emielhouman <emielhouman@gmail.com>
2024-01-11 17:55:46 +0100

    created a products and details screen + imported Native Stack Navigator

---
+++ screens/DetailsScreen.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+
+const ProductsScreen = ({ navigation }) => {
+    return (
+        <View>
+            <Text>Hello World!</Text>
+        </View>
+    );
+};
+
+export default ProductsScreen;
